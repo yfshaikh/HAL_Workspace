d9efce0dc2e3ecab0063915171d095efcde8d624
Yusuf <yshaikh113@outlook.com>
2026-05-03 09:58:08 -0500

    init new uart project

---
+++ 02_UART_TX_Polling/Debug/Src/main.d
@@ -0,0 +1,49 @@
+Src/main.o: ../Src/main.c \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ ../Inc/stm32f4xx_hal_conf.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rtc.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rtc_ex.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_usart.h
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+../Inc/stm32f4xx_hal_conf.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rtc.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rtc_ex.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/02_UART_TX_Polling/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_usart.h:
