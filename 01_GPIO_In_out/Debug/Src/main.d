cdc619277fc3454f04f6c0ee0c94b3b3057038c5
Yusuf <yshaikh113@outlook.com>
2026-05-03 09:34:34 -0500

    GPIO driver

--- 01_GPIO_In_out/Debug/Src/main.d
+++ 01_GPIO_In_out/Debug/Src/main.d
@@ -1,1 +1,59 @@
-Src/main.o: ../Src/main.c
+Src/main.o: ../Src/main.c \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ ../Inc/stm32f4xx_hal_conf.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/CMSIS/Include/core_cm4.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/CMSIS/Include/cmsis_version.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/CMSIS/Include/cmsis_compiler.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/CMSIS/Include/mpu_armv7.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rtc.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rtc_ex.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h \
+ /Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+../Inc/stm32f4xx_hal_conf.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/CMSIS/Include/core_cm4.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/CMSIS/Include/cmsis_version.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/CMSIS/Include/cmsis_compiler.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/CMSIS/Include/cmsis_gcc.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/CMSIS/Include/mpu_armv7.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rtc.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rtc_ex.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h:
+/Users/yusufshaikh/STM32CubeIDE/HAL_Workspace/01_GPIO_In_out/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
--- 01_GPIO_In_out/Src/main.c
+++ 01_GPIO_In_out/Src/main.c
@@ -1,10 +1,66 @@
+#include "stm32f4xx_hal.h"
+
+#define BTN_PIN GPIO_PIN_13
+#define BTN_PORT GPIOC
+#define LED_PIN GPIO_PIN_5
+#define LED_PORT GPIOA
+
+// BTN = PC13, BUS = AHB1EN bit0
+// LED = PA5, BUS = AHB1EN bit2
+
+
+void pc13_btn_init(void);
+void pa5_led_init(void);
 
 
 
-int counter;
+uint8_t buttonStatus;
 
 int main() {
+  HAL_Init();
+  pc13_btn_init();
+  pa5_led_init();
+
   while (1) {
-    counter++;
+    buttonStatus = HAL_GPIO_ReadPin(BTN_PORT, BTN_PIN);
+    HAL_GPIO_WritePin(LED_PORT, LED_PIN, buttonStatus);
   }
+
+  
+}
+
+
+void pc13_btn_init(void) {
+  // configure PC13 as input pin
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  GPIO_InitStruct.Pin = BTN_PIN;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+}
+
+void pa5_led_init(void) {
+  // configure PA5 as output pin
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  GPIO_InitStruct.Pin = LED_PIN;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+
+  // initialize GPIOA port with the configuration specified in GPIO_InitStruct
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+}
+
+
+void SysTick_Handler(void) {
+  // whenever there is a SysTick interrupt, this function will be called. 
+  // This function is defined weakly in the HAL library, so we can override it here to provide our own implementation. 
+  // In this case, we will just call the HAL_IncTick() function to increment the tick count, which is used for timing functions in the HAL library.
+  // a SysTick is a timer that generates an interrupt at regular intervals, which can be used for timing and scheduling tasks in an embedded system.
+  HAL_IncTick();
 }
