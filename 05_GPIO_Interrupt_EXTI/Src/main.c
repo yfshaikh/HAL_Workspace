6354421c6edd4c807845cf767ae4914c68a57353
Yusuf <yshaikh113@outlook.com>
2026-05-04 20:17:50 -0500

    gpio interrupt

---
+++ 05_GPIO_Interrupt_EXTI/Src/main.c
@@ -0,0 +1,25 @@
+#include "stm32f4xx_hal.h"
+#include <stdint.h>
+#include <stdio.h>
+
+int main() {
+  HAL_Init();
+
+
+  while (1) {
+
+  }
+  
+}
+
+
+
+
+void SysTick_Handler(void) {
+  // whenever there is a SysTick interrupt, this function will be called. 
+  // This function is defined weakly in the HAL library, so we can override it here to provide our own implementation. 
+  // In this case, we will just call the HAL_IncTick() function to increment the tick count, which is used for timing functions in the HAL library.
+  // a SysTick is a timer that generates an interrupt at regular intervals, which can be used for timing and scheduling tasks in an embedded system.
+  HAL_IncTick();
+}
+
