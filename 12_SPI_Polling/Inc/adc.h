77f03c6f1ab238455e8b1af8000fc291988e72a5
Yusuf <yshaikh113@outlook.com>
2026-05-08 07:05:53 -0500

    spi polling init

---
+++ 12_SPI_Polling/Inc/adc.h
@@ -0,0 +1,11 @@
+#ifndef ADC_H_
+#define ADC_H_
+
+uint32_t p0_adc_read(void);
+void adc_init_start(void);
+void adc_pa0_single_conversion_init(void);
+void adc_pa0_interrupt_init(void);
+
+
+
+#endif /* ADC_H_ */
---
+++ 12_SPI_Polling/Src/system_stm32f4xx.c
@@ -0,0 +1,8 @@
+#include <stdint.h>
+
+
+
+uint32_t SystemCoreClock = 16000000;
+const uint32_t AHBPrescTable[16] = {0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 6, 7, 8, 9};
+const uint32_t APBPrescTable[8]  = {0, 0, 0, 0, 1, 2, 3, 4};
+
