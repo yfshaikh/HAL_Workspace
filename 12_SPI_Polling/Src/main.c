659b5ab1fc5e27d5294f13fc1af1869a9d9a73cb
Yusuf <yshaikh113@outlook.com>
2026-05-08 07:09:16 -0500

    cleanup api polling

--- 12_SPI_Polling/Src/main.c
+++ 12_SPI_Polling/Src/main.c
@@ -1,22 +1,15 @@
 #include "stm32f4xx_hal.h"
 #include <stdint.h>
 #include <stdio.h>
+#include "adc.h"
+#include "uart.h"
 
-#define BTN_PORT GPIOC
-#define BTN_PIN GPIO_PIN_13
-#define LED_PORT GPIOD
-#define LED_PIN GPIO_PIN_12
 
-void gpio_pc13_interrupt_init(void);
+
+
 
 int main() {
   HAL_Init();
-  uart_init();
-  adc_pa0_interrupt_init(void);
-
-  // start ADC
-  // we have already configured the ADC to run in continuous conversion mode, so we just need to start it once and it will keep converting and generating interrupts whenever a conversion is complete.
-  HAL_ADC_Start_IT(&hadc1); // start ADC in interrupt mode, so that it will generate an interrupt whenever a conversion is complete and the data is ready to be read.
 
 
   while (1) {
@@ -25,14 +18,6 @@ int main() {
   
 }
 
-// automatically called by the HAL library when the ADC finishes a conversion (triggered by the interrupt handler)
-void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc) {
-    if (hadc->Instance == ADC1) { // check if the interrupt is from ADC1
-        uint32_t adc_value = p0_adc_read(); // read the converted value from the ADC data register using the p0_adc_read() function, which returns the value as a 32-bit unsigned integer.
-        printf("ADC Value: %lu\n", adc_value); // print the ADC value to the console using printf. %lu is used to format the unsigned long integer returned by p0_adc_read().
-    }
-
-}
 
 
 
